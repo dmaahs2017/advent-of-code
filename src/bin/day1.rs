a7cb2c877ce7eb759c6418047e601a923744ad6b
dalton <maahs2017@gmail.com>
2022-12-01 19:39:51 -0600

    d1p2 extract parse fn

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -4,48 +4,45 @@ use test::Bencher;
 
 fn main() {
     let input = include_str!("../../inputs/day1/input.txt");
-    println!("The elf with the most calories carries {} calories", solve_puzzle_1(input));
-    println!("The sum of 3 elves with the most calories is {}", solve_puzzle_2(input));
+    println!(
+        "The elf with the most calories carries {} calories",
+        solve_puzzle_1(input)
+    );
+    println!(
+        "The sum of 3 elves with the most calories is {}",
+        solve_puzzle_2(input)
+    );
+}
+
+/// Convert input &str into an iterator over each elf's total calories
+fn parse_input(input: &str) -> impl Iterator<Item = usize> + '_ {
+    input.split("\n\n").map(|elf| {
+        elf.split_whitespace()
+            .map(|c| c.parse::<usize>().unwrap())
+            .sum()
+    })
 }
 
 /// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 fn solve_puzzle_1(input: &str) -> usize {
-    input
-        .split("\n\n")
-        .map(|elf| {
-            elf.split_whitespace()
-                .map(|calories| calories.parse::<usize>().unwrap())
-                .sum()
-        })
-        .max()
-        .expect("input was empty")
+    parse_input(input).max().expect("input was empty")
 }
 
 /// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 fn solve_puzzle_2(input: &str) -> usize {
-    input
-        .split("\n\n")
-        .map(|elf| {
-            elf.split_whitespace()
-                .map(|calories| calories.parse::<usize>().unwrap())
-                .sum()
-        }).fold([0, 0, 0], |mut acc, elf_bag: usize| {
+    parse_input(input)
+        .fold([0, 0, 0], |mut acc, elf_bag: usize| {
             let min = acc.iter_mut().min().unwrap();
             *min = elf_bag.max(*min);
             acc
-        }).iter().sum()
+        })
+        .iter()
+        .sum()
 }
 
 #[allow(dead_code)]
 fn solve_puzzle_2_with_sorting(input: &str) -> usize {
-    let mut v = input
-        .split("\n\n")
-        .map(|elf| {
-            elf.split_whitespace()
-                .map(|calories| calories.parse::<usize>().unwrap())
-                .sum()
-        }).collect::<Vec<_>>();
-
+    let mut v = parse_input(input).collect::<Vec<_>>();
     v.sort();
     v.iter().rev().take(3).sum()
 }
@@ -73,6 +70,3 @@ fn solve_puzzle_1_works() {
     let input = include_str!("../../inputs/day1/sample.txt");
     assert_eq!(solve_puzzle_1(input), 24000)
 }
-
-
-
