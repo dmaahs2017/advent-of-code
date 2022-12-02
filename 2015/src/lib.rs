4837b0ce68d61ab02501a1ae2b47b07d55df6051
Dalton Maahs <dalton.maahs@yourbind.com>
2022-12-02 09:44:28 -0600

    2015 review

---
+++ 2015/src/lib.rs
@@ -0,0 +1,12 @@
+use std::fs;
+use std::io::read_to_string;
+
+pub fn read_input(day: u8) -> String {
+    let f = fs::File::open(format!("inputs/day{:0>2}/input.txt", day)).expect("input.txt not found");
+    read_to_string(f).expect("Failed to read input file to string")
+}
+
+pub fn read_sample(day: u8) -> String {
+    let f = fs::File::open(format!("inputs/day{:0>2}/sample.txt", day)).expect("input.txt not found");
+    read_to_string(f).expect("Failed to read input file to string")
+}
