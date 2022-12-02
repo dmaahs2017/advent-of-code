6e418d65d014379bed36734da959ce2a84e5a5e8
dalton <maahs2017@gmail.com>
2022-12-01 19:53:18 -0600

    refactor

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -5,11 +5,11 @@ fn main() {
     let input = include_str!("../../inputs/day1/input.txt");
     println!(
         "The elf with the most calories carries {} calories",
-        solve_puzzle_1(input)
+        p1::solve(input)
     );
     println!(
         "The sum of 3 elves with the most calories is {}",
-        solve_puzzle_2(input)
+        p2::solve(input)
     );
 }
 
@@ -22,29 +22,38 @@ fn parse_input(input: &str) -> impl Iterator<Item = usize> + '_ {
     })
 }
 
-/// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-fn solve_puzzle_1(input: &str) -> usize {
-    parse_input(input).max().expect("input was empty")
+/// Day 1 Puzzle 1
+pub mod p1 {
+    use super::*;
+    /// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+    pub fn solve(input: &str) -> usize {
+        parse_input(input).max().expect("input was empty")
+    }
 }
 
-/// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
-fn solve_puzzle_2(input: &str) -> usize {
-    parse_input(input)
-        .fold([0, 0, 0], |mut acc, elf_bag: usize| {
-            let min = acc.iter_mut().min().unwrap();
-            *min = elf_bag.max(*min);
-            acc
-        })
-        .iter()
-        .sum()
+/// day 1 Puzzle 2
+pub mod p2 {
+    use super::*;
+    /// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+    pub fn solve(input: &str) -> usize {
+        parse_input(input)
+            .fold([0, 0, 0], |mut acc, elf_bag: usize| {
+                let min = acc.iter_mut().min().unwrap();
+                *min = elf_bag.max(*min);
+                acc
+            })
+            .iter()
+            .sum()
+    }
+
+    #[cfg(test)]
+    pub fn solve_with_sorting(input: &str) -> usize {
+        let mut v = parse_input(input).collect::<Vec<_>>();
+        v.sort();
+        v.iter().rev().take(3).sum()
+    }
 }
 
-#[cfg(test)]
-fn solve_puzzle_2_with_sorting(input: &str) -> usize {
-    let mut v = parse_input(input).collect::<Vec<_>>();
-    v.sort();
-    v.iter().rev().take(3).sum()
-}
 
 #[cfg(test)]
 mod day1_tests {
@@ -52,27 +61,32 @@ mod day1_tests {
     use test::Bencher;
 
     #[bench]
-    fn bench_puzzle_2(b: &mut Bencher) {
-        let input = include_str!("../../inputs/day1/sample.txt");
-        b.iter(|| solve_puzzle_2(input))
+    fn bench_p2(b: &mut Bencher) {
+        let input = include_str!("../../inputs/day1/input.txt");
+        b.iter(|| p1::solve(input))
     }
 
     #[bench]
-    fn bench_puzzle_2_with_sorting(b: &mut Bencher) {
-        let input = include_str!("../../inputs/day1/sample.txt");
-        b.iter(|| solve_puzzle_2_with_sorting(input))
+    fn bench_p2_with_sorting(b: &mut Bencher) {
+        let input = include_str!("../../inputs/day1/input.txt");
+        b.iter(|| p2::solve_with_sorting(input))
     }
 
     #[test]
-    fn solve_puzzle_2_works() {
+    fn p2_works() {
         let input = include_str!("../../inputs/day1/sample.txt");
-        assert_eq!(solve_puzzle_2(input), 45000)
+        assert_eq!(p2::solve(input), 45000)
     }
 
     #[test]
-    fn solve_puzzle_1_works() {
+    fn p2_with_sorting_works() {
         let input = include_str!("../../inputs/day1/sample.txt");
-        assert_eq!(solve_puzzle_1(input), 24000)
+        assert_eq!(p2::solve_with_sorting(input), 45000)
+    }
+
+    #[test]
+    fn p1_works() {
+        let input = include_str!("../../inputs/day1/sample.txt");
+        assert_eq!(p1::solve(input), 24000)
     }
 }
-
