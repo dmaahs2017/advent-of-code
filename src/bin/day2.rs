9ff97db3861a3f4ab5edf439c2cf1a7e4d175192
dalton <maahs2017@gmail.com>
2022-12-02 00:21:25 -0600

    d2p2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -7,67 +7,90 @@ fn main() {
     println!("Part 2 answer = {}", p2::solve(input));
 }
 
-#[repr(i8)]
-#[derive(Eq, PartialEq, Clone, Copy, PartialOrd, Ord, Debug)]
-enum Shape {
-    Rock = 1,
-    Paper = 2,
-    Scisors = 3,
-}
-
-impl From<char> for Shape {
-    fn from(c: char) -> Shape {
-        match c {
-            'A' | 'X' => Shape::Rock,
-            'B' | 'Y' => Shape::Paper,
-            'C' | 'Z' => Shape::Scisors,
-            _ => unreachable!("Input data has no other values"),
-        }
-    }
-}
-
-/// Convert input &str an iter of scores
-fn parse_input(input: &str) -> impl Iterator<Item = (Shape, Shape)> + '_ {
+/// Parse input into an iterator over meaningful data
+fn parse_input(input: &str) -> impl Iterator<Item = (char, char)> + '_ {
     input.split('\n').filter(|s| s.len() >= 3).map(|round| {
         let round = round.as_bytes();
         let them = round[0] as char;
         let me = round[2] as char;
-        (them.into(), me.into())
+        (them, me)
     })
 }
 
-/// Day 1 Puzzle 1
+/// Score a round of rock paper scisors
+/// 1 = Rock, 2 = Paper, 3 = Scisors
+fn score(them: i8, me: i8) -> usize {
+    let mut score = me as usize;
+    match me - them {
+        1 | -2 => score += 6,
+        0 => score += 3,
+        -1 | 2 => {}
+        _ => panic!("Invalid input was given"),
+    }
+    score
+}
+
 pub mod p1 {
     use super::*;
-    /// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+    /// What would your total score be if everything goes exactly according to your strategy guide?
     pub fn solve(input: &str) -> usize {
         parse_input(input)
             .map(|(them, me)| {
-                let mut score = me as usize;
-                match me as i8 - them as i8 {
-                    1 | -2 => score += 6,
-                    0 => score += 3,
-                    -1 | 2 => {}
-                    _ => unreachable!("This should not be a possible score!"),
-                }
-                score
+                let them = them as i8 - 'A' as i8 + 1;
+                let me = me as i8 - 'W' as i8;
+                score(them, me)
             })
             .sum()
     }
 }
 
-/// day 1 Puzzle 2
 pub mod p2 {
     use super::*;
-    /// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+    /// Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
     pub fn solve(input: &str) -> usize {
-        todo!("Solve part 2")
+        parse_input(input)
+            .map(|(them, should_win)| {
+                let them = them as i8 - 'A' as i8 + 1;
+                let mut me = match should_win {
+                    'X' => them - 1,
+                    'Y' => them,
+                    'Z' => them + 1,
+                    _ => unreachable!("Should win field can only be Win, Lose, or Draw"),
+                };
+                if me == 0 {
+                    me = 3
+                }
+                if me == 4 {
+                    me = 1
+                }
+                score(them, me)
+            })
+            .sum()
     }
 }
 
 #[cfg(test)]
 mod day2_tests {
     use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_p1(b: &mut Bencher) {
+        let input = include_str!("../../inputs/day2/input.txt");
+        b.iter(|| p1::solve(input))
+    }
+
+    #[bench]
+    fn bench_p2(b: &mut Bencher) {
+        let input = include_str!("../../inputs/day2/input.txt");
+        b.iter(|| p2::solve(input))
+    }
+
+    #[test]
+    fn p2_works() {
+        let input = include_str!("../../inputs/day2/sample.txt");
+        assert_eq!(p2::solve(input), 12)
+    }
 
     #[test]
     fn p1_works() {
