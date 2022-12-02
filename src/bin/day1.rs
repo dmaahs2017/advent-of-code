bd5b0a9358b8f3cf22574869f7efd2c4823a4c2a
dalton <maahs2017@gmail.com>
2022-12-01 18:40:37 -0600

    day 1 part 2 without sorting

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("../../inputs/day1/input.txt");
-    println!("The elf with the most calories carries {} calories", solve_puzzle_1(input))
+    println!("The elf with the most calories carries {} calories", solve_puzzle_1(input));
+    println!("The sum of 3 elves with the most calories is {}", solve_puzzle_2(input));
 }
 
 /// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
@@ -18,15 +19,17 @@ fn solve_puzzle_1(input: &str) -> usize {
 
 /// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 fn solve_puzzle_2(input: &str) -> usize {
-    let mut v = input
+    input
         .split("\n\n")
         .map(|elf| {
             elf.split_whitespace()
                 .map(|calories| calories.parse::<usize>().unwrap())
                 .sum()
-        }).collect::<Vec<_>>();
-    v.sort();
-    v.iter().rev().take(3).sum()
+        }).fold([0, 0, 0], |mut acc, elf_bag: usize| {
+            let min = acc.iter_mut().min().unwrap();
+            *min = elf_bag.max(*min);
+            acc
+        }).iter().sum()
 }
 
 #[test]
