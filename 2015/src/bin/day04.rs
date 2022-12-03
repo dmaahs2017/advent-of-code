2a6f839c6616b7a5d9a74c7b068ded95061349bb
dalton <maahs2017@gmail.com>
2022-12-02 20:36:20 -0600

    d4

--- 2015/src/bin/day04.rs
+++ 2015/src/bin/day04.rs
@@ -1,7 +1,6 @@
 #![feature(test)]
 extern crate test;
 use aoc_2015::*;
-
 const DAY: u8 = 04;
 
 fn main() {
@@ -12,61 +11,32 @@ fn main() {
     );
 }
 
-pub mod p1 {
-    use std::process::Command;
-    pub fn solve(input: &str) -> usize {
-        for i in 0.. {
-            let input = format!("{}{}", input.trim(), i);
-            let hash = Command::new("md5")
-                .args(["-s".to_string(), input])
-                .output().unwrap().stdout;
-            let hash = String::from_utf8(hash).unwrap();
-            if i % 1000 == 0 {
-                println!("{}", hash.trim());
-            }
-            let (_, hash ) = hash.split_once('=').unwrap();
-            if hash.trim().chars().take(5).all(|c| c == '0') {
-                return i
-            }
-            
+fn start_with_zeroes(input: &str, n: usize) -> usize {
+    (0..).find_map(|i| {
+        let input = format!("{}{}", input.trim(), i);
+        let hash = format!("{:x}",md5::compute(input));
+        if hash.chars().take(n).all(|c| c == '0') {
+            Some(i)
+        } else {
+            None
         }
-        unreachable!()
+    }).unwrap()
+}
+
+pub mod p1 {
+    use super::*;
+    pub fn solve(input: &str) -> usize {
+        start_with_zeroes(input, 5)
     }
 }
 
 pub mod p2 {
+    use super::*;
     pub fn solve(input: &str) -> usize {
-        input.len()
+        start_with_zeroes(input, 6)
     }
 }
 
 #[cfg(test)]
 mod day04_tests {
-    use super::*;
-    use test::Bencher;
-    
-    const SAMPLE: &str = include_str!("../../inputs/day04/sample.txt");
-
-    //#[test]
-    //fn p1_works() {
-    //    assert_eq!(p1::solve(SAMPLE), 609043)
-    //}
-
-    //#[test]
-    //fn p2_works() {
-    //    assert_eq!(p2::solve(SAMPLE), 12)
-    //}
-
-
-    //#[bench]
-    //fn bench_p1(b: &mut Bencher) {
-    //    let input = &read_input(DAY);
-    //    b.iter(|| p1::solve(input))
-    //}
-
-    //#[bench]
-    //fn bench_p2(b: &mut Bencher) {
-    //    let input = &read_input(DAY);
-    //    b.iter(|| p2::solve(input))
-    //}
 }
