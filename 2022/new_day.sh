14de485cd6c4d991921ef44a83a75e335a27b977
dalton <maahs2017@gmail.com>
2022-12-04 00:24:19 -0600

    day 4

--- 2022/new_day.sh
+++ 2022/new_day.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
-cat template | sed "s/NN/$1/g" > src/bin/day$1.rs;
+no_leading_zero=$(echo $1 | sed 's/^0*//')
+cat template | sed -e "s/{NN}/$1/g" -e "s/{N}/$no_leading_zero/" > src/bin/day$1.rs;
 mkdir inputs/day$1;
 touch inputs/day$1/{input.txt,sample.txt};
+
+curl -b "session=$(cat session.txt)" "https://adventofcode.com/2022/day/$no_leading_zero/input" > inputs/day$1/input.txt
