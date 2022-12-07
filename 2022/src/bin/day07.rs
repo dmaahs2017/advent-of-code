528a2b3c21fc018f99b9ef646ed6fef7b32dda92
Dalton Maahs <dalton.maahs@yourbind.com>
2022-12-07 12:09:36 -0600

    refactor

--- 2022/src/bin/day07.rs
+++ 2022/src/bin/day07.rs
@@ -2,6 +2,7 @@
 extern crate test;
 use std::{collections::HashMap, path::PathBuf};
 
+use anyhow::{Context, Result};
 use aoc_2022::*;
 
 const DAY: u8 = 7;
@@ -16,40 +17,58 @@ fn main() {
     );
 }
 
-fn parse(input: &str) -> HashMap<PathBuf, usize> {
-    let mut map: HashMap<_, usize> = HashMap::new();
-    let mut cwd = PathBuf::new();
-    for line in input
+fn parse(input: &str) -> Result<HashMap<PathBuf, usize>> {
+    input
         .lines()
         .filter(|l| !l.starts_with("dir") && !l.starts_with("$ ls"))
-    {
-        if line.starts_with("$ cd") {
-            let new_dir = line.rsplit_once(" ").expect("should have a command").1;
-            if new_dir == ".." {
-                cwd.pop();
-            } else {
-                cwd = cwd.join(new_dir);
-            }
-        } else {
-            let bytes = line
-                .split_once(" ")
-                .expect("File should have bytes and file name")
-                .0
-                .parse::<usize>()
-                .expect("Bytes should be an integer");
+        .try_fold(
+            (HashMap::new(), PathBuf::new()),
+            |(mut map, mut cwd), line| {
+                if line.starts_with("$ cd") {
+                    let new_dir = line.get(5..).with_context(|| {
+                        format!("cd command has no target directory. Line: \"{}\"", line)
+                    })?;
 
-            for ancestor in cwd.ancestors() {
-                *map.entry(ancestor.to_path_buf()).or_default() += bytes;
-            }
-        }
-    }
-    map
+                    if new_dir.trim().is_empty() {
+                        return None.with_context(|| {
+                            format!("cd command target should not be blank. Line: \"{}\"", line)
+                        })?;
+                    }
+
+                    if new_dir == ".." {
+                        cwd.pop();
+                    } else {
+                        cwd = cwd.join(new_dir);
+                    }
+                } else {
+                    let bytes = line
+                        .split_once(" ")
+                        .with_context(|| {
+                            format!(
+                                "File entry should have bytes and file name. Line: \"{}\"",
+                                line
+                            )
+                        })?
+                        .0
+                        .parse::<usize>()
+                        .with_context(|| {
+                            format!("Bytes should be an integer. Line: \"{}\"", line)
+                        })?;
+
+                    for ancestor in cwd.ancestors() {
+                        *map.entry(ancestor.to_path_buf()).or_default() += bytes;
+                    }
+                }
+                Ok((map, cwd))
+            },
+        )
+        .map(|x| x.0)
 }
 
 pub mod p1 {
     use super::*;
     pub fn solve(input: &str) -> usize {
-        let map = parse(input);
+        let map = parse(input).expect("Failed to parse input");
         map.values()
             .filter_map(|&bytes| if bytes <= 100_000 { Some(bytes) } else { None })
             .sum()
@@ -60,7 +79,7 @@ pub mod p2 {
     use super::*;
     pub fn solve(input: &str) -> usize {
         const NEEDED_SPACE: usize = 30_000_000;
-        let map = parse(input);
+        let map = parse(input).expect("Failed to parse input");
         let used_space = map
             .get(&PathBuf::from(r"/"))
             .expect("Root dir should exist");
