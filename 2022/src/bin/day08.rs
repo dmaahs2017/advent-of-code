b5484e2df87628312aa8a3e27550ee65fad4400a
Dalton Maahs <dalton.maahs@yourbind.com>
2022-12-09 12:38:25 -0600

    finish day 8

--- 2022/src/bin/day08.rs
+++ 2022/src/bin/day08.rs
@@ -25,85 +25,135 @@ fn is_edge_visible(row: usize, col: usize, hm: &HeightMap) -> bool {
     if row == 0 || col == 0 {
         return true;
     }
-    if row == height - 1 || col == width-1 {
+    if row == height - 1 || col == width - 1 {
         return true;
     }
 
     let up = 0..row;
-    let down = row+1..height;
+    let down = row + 1..height;
     let left = 0..col;
-    let right = col+1..width;
+    let right = col + 1..width;
 
-    !up
-        .zip(iter::repeat(col))
+    !up.zip(iter::repeat(col))
         .any(|(x, y)| hm[x][y] >= max_tree_height)
-    || 
-
-    !down
-        .zip(iter::repeat(col))
-        .any(|(x, y)| hm[x][y] >= max_tree_height)
-    || 
-
-    !right
-        .zip(iter::repeat(row))
-        .any(|(y, x)| hm[x][y] >= max_tree_height)
-
-    || 
-
-    !left
-        .zip(iter::repeat(row))
-        .any(|(y, x)| hm[x][y] >= max_tree_height)
+        || !down
+            .zip(iter::repeat(col))
+            .any(|(x, y)| hm[x][y] >= max_tree_height)
+        || !right
+            .zip(iter::repeat(row))
+            .any(|(y, x)| hm[x][y] >= max_tree_height)
+        || !left
+            .zip(iter::repeat(row))
+            .any(|(y, x)| hm[x][y] >= max_tree_height)
 }
 
 fn scenic_score(row: usize, col: usize, hm: &HeightMap) -> usize {
-    let max_tree_height = hm[row][col];
-    let height = hm.len();
-    let width = hm[0].len();
+    let up = up_visibility(row, col, hm);
+    let down = down_visibility(row, col, hm);
+    let left = left_visibility(row, col, hm);
+    let right = right_visibility(row, col, hm);
 
+    up * down * left * right
+}
 
-    let up = 0..row;
-    let down = row+1..height;
-    let left = 0..col;
-    let right = col+1..width;
+fn up_visibility(row: usize, col: usize, hm: &HeightMap) -> usize {
+    let max_height = hm[row][col];
+    if row == 0 {
+        return 0;
+    }
 
-    (
-        up
+    let mut went_to_end = true;
+    let mut count = (0..row)
+        .rev()
         .zip(iter::repeat(col))
-        .position(|(x, y)| hm[x][y] >= max_tree_height).unwrap_or(row)
-    )
-        *
-    (
-        down
-        .zip(iter::repeat(col))
-        .position(|(x, y)| hm[x][y] >= max_tree_height).unwrap_or(height - row)
-    )
-    *
+        .map(|(x, y)| hm[x][y])
+        .take_while(|h| {
+            went_to_end = *h < max_height;
+            *h < max_height
+        })
+        .count();
+    if !went_to_end {
+        count += 1;
+    }
+    count
+}
 
+fn down_visibility(row: usize, col: usize, hm: &HeightMap) -> usize {
+    let max_height = hm[row][col];
+    if row == hm.len() - 1 {
+        return 0;
+    }
 
-    (
-        left
+    let mut went_to_end = true;
+    let mut count = (row + 1..hm.len())
+        .map(|x| hm[x][col])
+        .take_while(|h| {
+            went_to_end = *h < max_height;
+            *h < max_height
+        })
+        .count();
+    if !went_to_end {
+        count += 1;
+    }
+    count
+}
+
+fn left_visibility(row: usize, col: usize, hm: &HeightMap) -> usize {
+    let max_height = hm[row][col];
+    if col == 0 {
+        return 0;
+    }
+
+    let mut went_to_end = true;
+    let mut count = (0..col)
+        .rev()
         .zip(iter::repeat(row))
-        .position(|(y, x)| hm[x][y] >= max_tree_height).unwrap_or(col)
-    )
-    *
+        .map(|(y, x)| hm[x][y])
+        .take_while(|h| {
+            went_to_end = *h < max_height;
+            *h < max_height
+        })
+        .count();
+    if !went_to_end {
+        count += 1;
+    }
+    count
+}
 
+fn right_visibility(row: usize, col: usize, hm: &HeightMap) -> usize {
+    let max_height = hm[row][col];
+    if col == hm[0].len() - 1 {
+        return 0;
+    }
 
-    (
-        right
+    let mut went_to_end = true;
+    let mut count = (col + 1..hm[0].len())
         .zip(iter::repeat(row))
-        .position(|(y, x)| hm[x][y] >= max_tree_height).unwrap_or(width - col)
-    )
+        .map(|(y, x)| hm[x][y])
+        .take_while(|h| {
+            went_to_end = *h < max_height;
+            *h < max_height
+        })
+        .count();
+
+    if !went_to_end {
+        count += 1;
+    }
+    count
+}
+
+fn parse(input: &str) -> HeightMap {
+    input
+        .lines()
+        .map(|l| l.chars().filter_map(|c| c.to_digit(10)).collect())
+        .collect()
 }
 
 pub mod p1 {
     use super::*;
 
     pub fn solve(input: &str) -> usize {
-        let hm: HeightMap = input
-            .lines()
-            .map(|l| l.chars().filter_map(|c| c.to_digit(10)).collect())
-            .collect();
-
+        let hm = parse(input);
         let mut count = 0;
         for row in 0..hm.len() {
             for col in 0..hm[0].len() {
@@ -127,7 +177,7 @@ pub mod p2 {
         let mut score = 0;
         for row in 0..hm.len() {
             for col in 0..hm[0].len() {
-                score = scenic_score(row, col, &hm).max(score)
+                score = scenic_score(row, col, &hm).max(score);
             }
         }
         score
@@ -157,10 +207,51 @@ mod day08_tests {
     }
 
     #[test]
-    #[ignore]
+    fn up_visibility_works() {
+        let hm = parse(SAMPLE);
+        assert_eq!(up_visibility(2, 2, &hm), 1);
+        assert_eq!(up_visibility(1, 2, &hm), 1);
+        assert_eq!(up_visibility(0, 2, &hm), 0);
+        assert_eq!(up_visibility(1, 1, &hm), 1);
+        assert_eq!(up_visibility(2, 0, &hm), 2);
+        assert_eq!(up_visibility(4, 3, &hm), 4);
+        assert_eq!(up_visibility(3, 2, &hm), 2);
+    }
+
+    #[test]
+    fn down_visibility_works() {
+        let hm = parse(SAMPLE);
+        assert_eq!(down_visibility(2, 2, &hm), 1);
+        assert_eq!(down_visibility(1, 2, &hm), 2);
+        assert_eq!(down_visibility(0, 2, &hm), 1);
+        assert_eq!(down_visibility(4, 0, &hm), 0);
+        assert_eq!(down_visibility(3, 4, &hm), 1);
+    }
+
+    #[test]
+    fn left_visibility_works() {
+        let hm = parse(SAMPLE);
+        assert_eq!(left_visibility(2, 2, &hm), 1);
+        assert_eq!(left_visibility(1, 2, &hm), 1);
+        assert_eq!(left_visibility(0, 2, &hm), 2);
+        assert_eq!(left_visibility(4, 0, &hm), 0);
+        assert_eq!(left_visibility(0, 3, &hm), 3);
+    }
+
+    #[test]
+    fn right_visibility_works() {
+        let hm = parse(SAMPLE);
+        assert_eq!(right_visibility(2, 2, &hm), 1);
+        assert_eq!(right_visibility(1, 2, &hm), 2);
+        assert_eq!(right_visibility(0, 2, &hm), 1);
+        assert_eq!(right_visibility(4, 0, &hm), 1);
+        assert_eq!(right_visibility(0, 4, &hm), 0);
+    }
+
+    #[test]
     fn p2_input() {
         let input = &read_input(DAY);
-        assert_eq!(p2::solve(input), 0)
+        assert_eq!(p2::solve(input), 199272)
     }
 }
 
@@ -176,6 +267,7 @@ mod day08_benchmarks {
     }
 
     #[bench]
+    #[ignore]
     fn bench_p2(b: &mut Bencher) {
         let input = &read_input(DAY);
         b.iter(|| p2::solve(input))
