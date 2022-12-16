365b3ebd371600eab1f207980664edacf047a499
dalton <maahs2017@gmail.com>
2022-12-15 18:03:12 -0600

    day 15 progress kinda

--- 2022/src/bin/day15.rs
+++ 2022/src/bin/day15.rs
@@ -10,11 +10,11 @@ fn main() {
     let input = &read_input(DAY);
     println!(
         "Day {:0>2}: Part 1 answer = {}, Part 2 answer = {}",
-        DAY, p1::solve(input), p2::solve(input)
+        DAY, p1::solve(input, 2_000_000), p2::solve(input)
     );
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 struct Point {
     row: isize,
     col: isize,
@@ -29,12 +29,15 @@ impl Point {
 }
 
 /// Returns a list of Points and their manhattan distance to the nearest beacon
-fn parse(input: &str) -> Vec<(Point, usize)> {
+fn parse(input: &str) -> ( Vec<(Point, Point)>, isize, isize ) {
     let re = Regex::new(r"(-?\d+)").expect("Invalid regex");
-    input.lines()
+    let mut leftmost = isize::MAX;
+    let mut rightmost = isize::MIN;
+    let sensors = input.lines()
         .map(|line| {
             let values = re.captures_iter(line).map(|x| {
-                x[0].parse().unwrap()
+                let v: isize = x[0].parse().unwrap();
+                v
             }).collect::<Vec<_>>();
             let c1 = values[0];
             let r1 = values[1];
@@ -42,22 +45,36 @@ fn parse(input: &str) -> Vec<(Point, usize)> {
             let c2 = values[2];
             let r2 = values[3];
 
+            leftmost = leftmost.min(c1).min(c2);
+            rightmost = rightmost.max(c1).max(c2);
+
             let p1 = Point::new(r1, c1);
             let p2 = Point::new(r2, c2);
-            let distance = p1.distance(&p2);
-            (p1, distance)
-        }).collect()
+            (p1, p2)
+        }).collect();
+
+
+
+    (sensors, leftmost * 4, rightmost * 4)
 }
 
 pub mod p1 {
+    // 5_289_249
     use super::*;
-    pub fn solve(input: &str) -> usize {
-        let sensors = parse(input);
-        let row = 10;
-        (isize::MIN..=isize::MAX).filter(|col| {
+    pub fn solve(input: &str, row: isize) -> usize {
+        let ( sensors, leftmost, rightmost ) = parse(input);
+        (leftmost..=rightmost).filter(|col| {
             let p = Point::new(row, *col);
-            sensors.iter().any(|sensor| p.distance(&sensor.0) <= sensor.1)
-        }).count()
+
+            // If a point on the row is within the range of any sensors
+            // and that point is not a beacon, then count it
+            sensors.iter().any(|sensor| { 
+                p.distance(&sensor.0) <=  sensor.0.distance(&sensor.1)
+                    && p != sensor.1
+            })
+        })
+        //.inspect(|v| println!("{:?}", Point::new(row, *v)))
+        .count()
     }
 }
 
@@ -75,14 +92,13 @@ mod day15_tests {
 
     #[test]
     fn p1_sample() {
-        assert_eq!(p1::solve(SAMPLE), 26)
+        assert_eq!(p1::solve(SAMPLE, 10), 26)
     }
 
     #[test]
-    #[ignore]
     fn p1_input() {
         let input = &read_input(DAY);
-        assert_eq!(p1::solve(input), 0)
+        assert_eq!(p1::solve(input, 2_000_000), 0)
     }
 
     #[test]
@@ -107,12 +123,14 @@ mod day15_benchmarks {
     use test::Bencher;
 
     #[bench]
+    #[ignore]
     fn bench_p1(b: &mut Bencher) {
         let input = &read_input(DAY);
-        b.iter(|| p1::solve(input))
+        b.iter(|| p1::solve(input, 2_000_000))
     }
 
     #[bench]
+    #[ignore]
     fn bench_p2(b: &mut Bencher) {
         let input = &read_input(DAY);
         b.iter(|| p2::solve(input))
