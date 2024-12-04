58d19d97ae78ba6d34916783e1baf5c320624fb3
Dalton Maahs <dalton.maahs@surest.com>
2024-12-04 12:00:39 -0600

    day 3

--- 2024/src/bin/day03.rs
+++ 2024/src/bin/day03.rs
@@ -1,9 +1,12 @@
 #![feature(test)]
 extern crate test;
 use aoc_2024::*;
-use regex::Regex;
-use nom::{character::complete as character, multi::*, sequence::*, branch::*, IResult, bytes::complete as bytes, Parser};
+use nom::{
+    branch::*, bytes::complete as bytes, character::complete as character, multi::*, sequence::*,
+    IResult, Parser,
+};
 use nom_supreme::ParserExt;
+use regex::Regex;
 
 const DAY: u8 = 3;
 
@@ -11,18 +14,22 @@ fn main() {
     let input = &read_input(DAY);
     println!(
         "Day {:0>2}: Part 1 answer = {}, Part 2 answer = {}",
-        DAY, solve_p1(input), solve_p2(input)
+        DAY,
+        solve_p1(input),
+        solve_p2(input)
     );
 }
 
-
 pub fn solve_p1(input: &str) -> i64 {
     let r = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    r.captures_iter(input).map(|c| c.extract().1).map(|s:[&str; 2]| { 
-        let a = s[0].parse::<i64>().unwrap() ;
-        let b = s[1].parse::<i64>().unwrap() ;
-        a * b
-    }).sum()
+    r.captures_iter(input)
+        .map(|c| c.extract().1)
+        .map(|s: [&str; 2]| {
+            let a = s[0].parse::<i64>().unwrap();
+            let b = s[1].parse::<i64>().unwrap();
+            a * b
+        })
+        .sum()
 }
 
 #[derive(Debug)]
@@ -34,14 +41,19 @@ enum Instruction {
 
 fn parse(s: &str) -> IResult<&str, Vec<Instruction>> {
     many1(alt((
-        bytes::tag("mul(").precedes(separated_pair(character::i64, bytes::tag(","), character::i64)).terminated(bytes::tag(")")).map(|s| Some(Instruction::Mul(s.0, s.1))),
-        bytes::tag("don't").map(|_| Some( Instruction::Dont )),
-        bytes::tag("do").map(|_| Some( Instruction::Do )),
-        bytes::take(1usize).map(|_| None)
-    )))(s).map(|x| {
-        (x.0, x.1.into_iter().filter_map(|a| a).collect())
-    })
-
+        bytes::tag("mul(")
+            .precedes(separated_pair(
+                character::i64,
+                bytes::tag(","),
+                character::i64,
+            ))
+            .terminated(bytes::tag(")"))
+            .map(|s| Some(Instruction::Mul(s.0, s.1))),
+        bytes::tag("don't").map(|_| Some(Instruction::Dont)),
+        bytes::tag("do").map(|_| Some(Instruction::Do)),
+        bytes::take(1usize).map(|_| None),
+    )))(s)
+    .map(|x| (x.0, x.1.into_iter().flatten().collect()))
 }
 pub fn solve_p2(input: &str) -> i64 {
     let instrucitons = parse(input).unwrap().1;
@@ -52,18 +64,21 @@ pub fn solve_p2(input: &str) -> i64 {
         match *i {
             Instruction::Dont => do_flag = false,
             Instruction::Do => do_flag = true,
-            Instruction::Mul(a, b) => if do_flag {acc += a * b }
+            Instruction::Mul(a, b) => {
+                if do_flag {
+                    acc += a * b
+                }
+            }
         }
     }
 
     acc
-
 }
 
 #[cfg(test)]
 mod day03_tests {
     use super::*;
-    
+
     const SAMPLE: &str = include_str!("../../inputs/day03/sample.txt");
     const SAMPLE2: &str = include_str!("../../inputs/day03/sample2.txt");
 
@@ -89,8 +104,6 @@ mod day03_tests {
         let input = &read_input(DAY);
         assert_eq!(solve_p2(input), 93729253)
     }
-
-
 }
 
 #[cfg(test)]
